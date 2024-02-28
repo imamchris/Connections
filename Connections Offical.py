404a042eea3e9aa98d727a05e3708648576b5b10
cpereira <cpereira@student.saintaug.nsw.edu.au>
2024-02-28 15:20:28 +1100

    Establish a basic scaffold for what the game should (or may) look like

--- Connections Offical.py
+++ Connections Offical.py
@@ -1,24 +1,42 @@
-def get_category():
-    print("get_category")
+def get_category(): # stores/retrivies words from dictionaries 
+    categories = [0, 70, 60]
+
+    return categories
 
 
-def play_game():
+def play_game(): # main game loop 
+    display_category()
     lives = 4
     won = False
-    display_category()
     while lives >= 0 and won == False:
         get_words()
 
+def display_category(): #creates the grid with a random selection of categories
+    # grid = []
+    # row_index = 0
 
-def display_category():
-    print("grid")
+    # for connection in categories:
+    #     col_index = 0
+    #     for word in connection: # ['words']: # within dictionary, get each word
+    #         # put the word inside the correct grid reference
+    #         categories[row_index][col_index] = word
+    #         grid.append(word)
+    #         col_index = col_index + 1 # moves to next column
+    #     row_index = row_index + 1 # moves to the next row
+
+    # print(grid)
+
+    
 
 
 def get_words():
     guess = print(input("What is your input? "))
+    return guess
+    
 
-    if guess == True:
-        print("Yay")
+
+def check_answer():
+    print("Check Answers")
 
 
 play_game() 
